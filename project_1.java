ed77f3d16eeeeaa6fdc8d504825b78b4a2deb328
Nursultan Makhmutov <241564@astanait.ed.kz>
2025-09-13 14:11:19 +0500

    index.html

---
+++ project_1.java
@@ -0,0 +1,13 @@
+public class Asik {
+    String hat;
+    String glasses;
+    String nose;
+    String eye;
+    String fingers;
+    String tshirt;
+    String Kazakh;
+    String Name;
+    String Surname;
+    String age;
+    public Asik (String hat)
+}
